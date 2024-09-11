b73e6174d9674713e73326a37e580926f5feba65
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-09-11 20:31:22 +0200

    Ejercicios 1_1 y 2_1

---
+++ Assets/Scripts/Ejercicio2_1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    int edad,codigoPostal,numeroDeHijos,tallaCamisa,mayorEdad,minutos,dias;
+    float altura,iva,peso,precio,contador;
+    char genero,letra,tallaCamiseta;
+    string nombre,mensaje,matricula;
+    bool alumnoRepetir;
+    bool mayorDeEdad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Ejercico1_1.cs
+++ Assets/Scripts/Ejercico1_1.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Ejercico1_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Declarar variables. Tipo, Nombre y Valor.
+    //public int vida = 5;
+    //public float porcentajeAcierto = 3.45f;
+    //public bool quemado = true;
+    //string explorador = "Iñigo Montoya";
+    //char solucionPuzzle = 'A';
+    
+
+
     void Start()
     {
-        
+        //vida = 5; //Asignar el valor a una variable.
+        Debug.Log("¡Hola Mundo!");
+        Debug.Log("Este es el primer juego de Adrian(el Abuelo)");
+        Debug.Log("Estoy aprendiendo C#");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame"); //Este mensaje aparece muchas veces porque cada frame lo crea, no solo al inicio como en el start.
     }
 }
