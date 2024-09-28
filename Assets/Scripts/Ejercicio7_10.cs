daa5feb6cbefc11f7c938ef1a343c2efe7e112f2
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-09-28 15:28:45 +0200

    Algunos ejercicios mas del apartado 7

--- Assets/Scripts/Ejercicio7_10.cs
+++ Assets/Scripts/Ejercicio7_10.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ejercicio7_10 : MonoBehaviour
 {
+    //No se como realizar este ejercicio con lo que hemos aprendido, sin usar el Mathf.Min o Max
+    [SerializeField] int numero1, numero2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (numero1 = 1; numero1 <= numero2; numero1++) //Esto me sirve para leer los numero pares, pero se va a saltar el resto
+        {
+            if (numero1 % 2 == 0)
+            {
+                Debug.Log(numero1);
+            }
+
+        }
     }
 
     // Update is called once per frame
