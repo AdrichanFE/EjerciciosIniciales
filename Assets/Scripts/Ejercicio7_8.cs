7df128d227004c3d10fa65169d7f11b7eac9426b
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-09-28 13:44:10 +0200

    Yes

--- Assets/Scripts/Ejercicio7_8.cs
+++ Assets/Scripts/Ejercicio7_8.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class Ejercicio7_8 : MonoBehaviour
 {
+    
     [SerializeField] int numero1, numero2;
     // Start is called before the first frame update
     void Start()
     {
 
-        if (numero1 %2==0 && numero1<numero2)
-        {
-            for (numero1 = numero1; numero1 < numero2; numero1%=2)
+            for (numero1 = 1; numero1 <= numero2; numero1++)
             {
+                if (numero1 % 2 == 0)
+                {
                 Debug.Log(numero1);
-
+                }
+                
             }
-        }
-        else if (numero2 % 2 == 0 && numero2 < numero1)
-        {
-            for (numero2 = numero2; numero2 < numero1; numero2%=2)
-            {
-                Debug.Log(numero2);
-            }
-        }
+       
+        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Ejercicio7_9.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_9 : MonoBehaviour
+{
+
+    [SerializeField] int numero1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <=numero1 ; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Debug.Log(i);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
